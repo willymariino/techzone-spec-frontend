6268e1c372c8589527de073e527ded1a02549682
willymariino <wg.mariino@gmail.com>
2025-09-29 20:37:17 +0200

    feat(footer): enhance layout and responsiveness of Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,39 +11,53 @@ function Footer() {
 
         <>
             <footer className="footer">
+
                 <div className="footer-top">
-                    <div className="footer-brand">
-                        <h2>TechZone</h2>
-                        <p>La tua tecnologia di fiducia</p>
+
+                    <div className="footer-left">
+
+                        <div className="footer-brand">
+                            <h2 className="Header-title">TechZone</h2>
+                            <p>La tua tecnologia di fiducia</p>
+                        </div>
+
+                        <div>
+                            <h4>Supporto</h4>
+
+                            <nav className="footer-link">
+
+                                <NavLink to="#" className="nav-link"> FAQ </NavLink>
+                                <NavLink to="#" className="nav-link">Contatti </NavLink>
+                                <NavLink to="#" className="nav-link">Resi & Spedizioni</NavLink>
+
+                            </nav>
+
+                        </div>
+
                     </div>
-                    <div>
-                        <h4>Supporto</h4>
 
-                        <nav className="footer-link">
-
-                            <NavLink to="#" className="nav-link"> FAQ </NavLink>
-                            <NavLink to="#" className="nav-link">Contatti </NavLink>
-                            <NavLink to="#" className="nav-link">Resi & Spedizioni</NavLink>
-
-                        </nav>
-                    </div>
                     <div className="footer-newsletter">
-                        <h4>Iscriviti</h4>
+                        <h4>Iscriviti alla nostra newsletter</h4>
                         <input type="email" placeholder="La tua email" />
                         <button>Invia</button>
                     </div>
+
                 </div>
 
                 <div className="footer-bottom">
+
                     <p>© 2025 TechZone. Tutti i diritti riservati.</p>
+
                     <div className="social-icons">
-                        <strong> <p> join us </p></strong>
-                        <a href="#" className="nav-link"><FaInstagram /></a>
-                        <a href="#" className="nav-link"><FaFacebook /></a>
-                        <a href="#" className="nav-link"><FaXTwitter /></a>
-                        <a href="#" className="nav-link"> <FaYoutube /></a>
+                        <strong> <p> follow us </p></strong>
+                        <a href="#" className="nav-link icon"><FaInstagram /></a>
+                        <a href="#" className="nav-link icon"><FaFacebook /></a>
+                        <a href="#" className="nav-link icon"><FaXTwitter /></a>
+                        <a href="#" className="nav-link icon"> <FaYoutube /></a>
                     </div>
+
                 </div>
+
             </footer>
 
 
